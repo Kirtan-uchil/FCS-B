29bc8b7174c19542bf278be1dd362c411be4a637
Kirtan-uchil <73773789+Kirtan-uchil@users.noreply.github.com>
2025-06-05 19:41:02 +0930

    question16_ii.sh

--- question16_ii.sh
+++ question16_ii.sh
@@ -1,7 +1,5 @@
-
 cp -r dir16_1 dir16_1_copy
 mv dir16_1/dir16_2/file16_2 dir16_1/dir16_2/file16_2_renamed
-Move dir16_4 up one level in the original dir16_1 (from dir16_3 to dir16_2)
 mv dir16_1/dir16_2/dir16_3/dir16_4 dir16_1/dir16_2/
 rmdir dir16_1/dir16_2/dir16_3
 rm dir16_1/dir16_5/file16_4
